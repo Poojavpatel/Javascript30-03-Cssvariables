43f6feda197d16f1e43ae9aeea30fc354f8441d1
Pooja patel <poojavpatel71@gmail.com>
2018-07-28 00:06:24 +0530

    js almost done

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hekkop");
+/*jshint esversion: 6 */
+const inputs = document.querySelectorAll('.controls input');
+console.log(inputs);
+
+function handleUpdate() {
+    //console.log(this.value);
+    const suffix = this.dataset.sizing || '' ;
+    console.log(suffix);
+    console.log(this.name);
+    console.log(this.value);
+    document.documentElement.style.setProperty(` --${this.name} ` , this.value + suffix );
+}
+
+inputs.forEach(input => {
+    input.addEventListener('change' , handleUpdate);
+});
+// inputs.forEach(input => {
+//     input.addEventListener('mousemove' , handleUpdate);
+// });
